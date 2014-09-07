8f6d0b822882f12b81b72e82e5b35b1c79fd2f46
kobe lipkens <kobe@wieni.be>
2014-09-07 14:39:40 +0200

    unittests: start php webserver to dramatically increase testspeed assertZipIntegrity now compares zip contents to raw file contents mock time() test validation exceptions

--- tests/ZipStreamerTest.php
+++ tests/ZipStreamerTest.php
@@ -1,51 +1,88 @@
 <?php
+
+namespace ZipStreamer;
+
+use ZipStreamer\Tests\ZipStreamerTest;
+
+/**
+ * Mocks php time() calls in the ZipStreamer namespace.
+ */
+function time() {
+  return ZipStreamerTest::mockTime();
+}
+
 namespace ZipStreamer\Tests;
 
-use Symfony\Component\Yaml\Exception\RuntimeException;
 use ZipStreamer\Outputs\File;
 use ZipStreamer\ZipStreamer;
 
 class ZipStreamerTest extends \PHPUnit_Framework_TestCase {
 
-  const BLOCK_SIZE = 102400;
+  const BLOCK_SIZE = 10240;
   protected $destination;
-  protected $output;
-  protected $zipArchiveErrs = array();
+
+  protected static $time = 0;
+  protected static $zipArchiveErrs = array();
+  protected static $fileContents = array();
+
+  public static function mockTime() {
+    return self::$time;
+  }
+
+  public static function setUpBeforeClass() {
+    $reflection = new \ReflectionClass('\ZipArchive');
+    $constants = $reflection->getConstants();
+    foreach ($constants as $constant => $value) {
+      if (substr($constant, 0, 3) == 'ER_') self::$zipArchiveErrs[$value] = $constant;
+    }
+  }
 
   public function setUp() {
-    $this->destination = tempnam(sys_get_temp_dir(), 'zip-steamer');
+    $this->destination = tempnam(sys_get_temp_dir(), ZIPSTREAMER_TMPFILE_PREFIX);
     touch($this->destination);
     if (!is_writable($this->destination)) {
       throw new \RuntimeException(sprintf('%s is not writable', $this->destination));
     }
-
-    $reflection = new \ReflectionClass('\ZipArchive');
-    $constants = $reflection->getConstants();
-    foreach ($constants as $constant => $value) {
-      if (substr($constant, 0, 3) == 'ER_') $this->zipArchiveErrs[$value] = $constant;
-    }
   }
 
   public function fileProvider() {
-    $remoteUris = array('http://lorempixel.com/100/100/', 'http://lorempixel.com/50/50/', 'http://www.randomtext.me/download/txt/gibberish/p-14/20-2000');
+    $localUris = array();
+    $remoteUris = array();
+    $files = glob(WEB_SERVER_DOCROOT . '/*');
+    foreach ($files as $i => $file) {
+      if (!is_file($file)) continue;
+      $localUris[] = $file;
+      isset(self::$fileContents[$i]) || self::$fileContents[] = file_get_contents($file);
+      $remoteUris[] = 'http://' . WEB_SERVER_HOST . ':' . WEB_SERVER_PORT . '/' . basename($file);
+    }
 
     $data = array();
-    $data[] = array($remoteUris, TRUE);
+    $data[] = array($localUris, TRUE, time());
+    $data[] = array($remoteUris, TRUE, -10000);
+    $data[] = array(array_map(array($this, 'fropen'), $localUris), TRUE);
     $data[] = array(array_map(array($this, 'fropen'), $remoteUris), TRUE);
-    $data[] = array($remoteUris, FALSE);
-    $data[] = array(array_map(array($this, 'fropen'), $remoteUris), FALSE);
 
-    $localUris = array_map(array($this, 'fetch'), $remoteUris);
-    $data[] = array($localUris, TRUE);
     $data[] = array($localUris, FALSE);
-
+    $data[] = array($remoteUris, FALSE);
+    $data[] = array(array_map(array($this, 'fropen'), $localUris), FALSE);
+    $data[] = array(array_map(array($this, 'fropen'), $remoteUris), FALSE, time());
     return $data;
+  }
+
+  protected function getFileContents($dest) {
+    $contents = array();
+    foreach (self::$fileContents as $i => $content) {
+      $contents[$dest . '/' . $i] = $content;
+    }
+    return $contents;
   }
 
   /**
    * @dataProvider    fileProvider
    */
-  public function testZipIntegrityStore(array $files, $continuous) {
+  public function testZipIntegrityStore(array $files, $continuous, $time = 0) {
+    self::$time = $time;
+
     $out = new File($this->destination, TRUE);
     $zip = new ZipStreamer($out, self::BLOCK_SIZE, $continuous);
     for ($i = 0; $i < count($files); $i++) {
@@ -53,58 +90,99 @@ class ZipStreamerTest extends \PHPUnit_Framework_TestCase {
       $zip->send();
     }
     $zip->flush();
-    $this->assertZipFileIntegrity($this->destination);
+    $this->assertZipFileIntegrity($this->destination, $this->getFileContents('stored'));
   }
 
   /**
    * @dataProvider    fileProvider
    */
-  public function testZipIntegritySoftDeflate(array $files, $continuous) {
+  public function testZipIntegritySoftDeflate(array $files, $continuous, $time = 0) {
+    self::$time = $time;
+
     $out = new File($this->destination, TRUE);
     $zip = new ZipStreamer($out, self::BLOCK_SIZE, $continuous);
     for ($i = 0; $i < count($files); $i++) {
-      $zip->add('soft-deflate/' . $i, $files[$i], 1);
+      $zip->add('dir/dir/soft-deflate/' . $i, $files[$i], 1);
       $zip->send();
     }
     $zip->flush();
-    $this->assertZipFileIntegrity($this->destination);
+    $this->assertZipFileIntegrity($this->destination, $this->getFileContents('dir/dir/soft-deflate'));
   }
 
   /**
    * @dataProvider    fileProvider
    */
-  public function testZipIntegrityHardDeflate(array $files, $continuous) {
+  public function testZipIntegrityHardDeflate(array $files, $continuous, $time = 0) {
+    self::$time = $time;
+
     $out = new File($this->destination, TRUE);
     $zip = new ZipStreamer($out, self::BLOCK_SIZE, $continuous);
     for ($i = 0; $i < count($files); $i++) {
       $zip->add('hard-deflate/' . $i, $files[$i], 9);
-      $zip->send();
     }
     $zip->flush();
-    $this->assertZipFileIntegrity($this->destination);
+    $this->assertZipFileIntegrity($this->destination, $this->getFileContents('hard-deflate'));
   }
 
-  private function assertZipFileIntegrity($zipFile) {
+  /**
+   * @group exceptions
+   */
+  public function testInvalidFiles() {
+    $out = new File($this->destination, TRUE);
+    $zip = new ZipStreamer($out, self::BLOCK_SIZE);
+    $e = NULL;
+    try {
+      $zip->add('invalid/file', '/fake/should-not-exist');
+    } catch (\Exception $e) {
+      $this->assertRegExp('/file .*? is not readable/', $e->getMessage());
+    }
+    $this->assertInstanceOf('\RunTimeException', $e);
+
+    $e = NULL;
+    try {
+      $zip->add('', '');
+    } catch (\Exception $e) {
+      $this->assertEquals('File destination should be a non-empty string', $e->getMessage());
+    }
+    $this->assertInstanceOf('\InvalidArgumentException', $e);
+
+    $e = NULL;
+    try {
+      $zip->add('invalid/file', '');
+    } catch (\Exception $e) {
+      $this->assertEquals('path should be a non-empty string', $e->getMessage());
+    }
+    $this->assertInstanceOf('\InvalidArgumentException', $e);
+
+    $e = NULL;
+    try {
+      $zip->add('invalid/file', array('abc'));
+    } catch (\Exception $e) {
+      $this->assertEquals('path should be a non-empty string', $e->getMessage());
+    }
+    $this->assertInstanceOf('\InvalidArgumentException', $e);
+
+    $e = NULL;
+    $file = tempnam(sys_get_temp_dir(), ZIPSTREAMER_TMPFILE_PREFIX);
+    touch($file);
+    try {
+      $zip->add('invalid/file2', fopen($file, 'w'));
+    } catch (\Exception $e) {
+      $this->assertEquals('Resource should be readable', $e->getMessage());
+    }
+    $this->assertInstanceOf('\InvalidArgumentException', $e);
+  }
+
+  protected function assertZipFileIntegrity($zipFile, array $fileContents) {
     $phpZip = new \ZipArchive();
-    $err = $phpZip->open($this->destination, \ZipArchive::CHECKCONS);
-    $this->assertTrue($err, sprintf('Integrity check error: %s', isset($this->zipArchiveErrs[$err]) ? $this->zipArchiveErrs[$err] : 'Unknown'));
+    $err = $phpZip->open($zipFile, \ZipArchive::CHECKCONS);
+    $this->assertTrue($err, sprintf('Integrity check error: %s', isset(self::$zipArchiveErrs[$err]) ? self::$zipArchiveErrs[$err] : 'Unknown'));
+    foreach ($fileContents as $dest => $content) {
+      $this->assertEquals($content, $phpZip->getFromName($dest), $dest);
+    }
   }
 
-  private function fropen($file) {
+  protected function fropen($file) {
     return fopen($file, 'rb');
   }
-
-  private function fetch($uri) {
-    $dest = tempnam(sys_get_temp_dir(), 'zip-steamer-asset');
-    touch($dest);
-    if (($data = file_get_contents($uri)) === FALSE) {
-      throw new RuntimeException(sprintf('Could not fetch %s', $uri));
-    }
-    if (file_put_contents($dest, $data) === FALSE) {
-      throw new RuntimeException(sprintf('Could not write %s to %s', $uri, $dest));
-    }
-
-    return $dest;
-  }
-
 }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,35 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+// Props: http://tech.vg.no/2013/07/19/using-phps-built-in-web-server-in-your-test-suites/
+function startPhpFileServer($host, $port, $docroot) {
+  $command = sprintf('php -S %s:%d -t %s >/dev/null 2>&1 & echo $!', $host, $port, $docroot);
+
+  $output = array();
+  exec($command, $output);
+  $pid = (int) $output[0];
+  echo sprintf('Web server started on %s:%d with PID %d', $host, $port, $pid) . PHP_EOL;
+
+  // Wait for the server to start.
+  $files = glob($docroot . '/*');
+  $file = reset($files);
+  if ($file === FALSE) {
+    throw new \RuntimeException('Can not determine file server status');
+  }
+  while (($fh = @fopen('http://' . $host . ':' . $port . '/' . basename($file), 'r')) === FALSE) {
+    usleep(10000);
+  }
+  fclose($fh);
+
+  register_shutdown_function(function () use ($pid) {
+    echo sprintf('Killing webserver process with ID %d', $pid) . PHP_EOL;
+    shell_exec('kill ' . $pid);
+  });
+}
+
+$loader = require __DIR__ . '/../vendor/autoload.php';
+register_shutdown_function(function () {
+  $files = glob(sys_get_temp_dir() . '/' . ZIPSTREAMER_TMPFILE_PREFIX . '*');
+  array_map('unlink', $files);
+});
+
+startPhpFileServer(WEB_SERVER_HOST, WEB_SERVER_PORT, WEB_SERVER_DOCROOT);
